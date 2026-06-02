a5138507a226d3813781136f5e4ad8ceadb07022
Neidi387 <neidi387@googlemail.com>
2026-06-02 07:16:14 -0500

    Initial

---
+++ portfolio/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>neidenberger.dev</title>
+	<meta name="description" content="neidenberger.dev" />
+</svelte:head>
+
+<section>
+	<a href="http://seflie.neidenberger.dev/">seflie.neidenberger.dev</a>
+</section>
+
+<style>
+	section {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
